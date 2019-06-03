109604d037a13781803c01c6e93e7a911c1562b5
Crystal Martin <drcrystalm@gmail.com>
2019-06-02 22:11:18 -0500

    what a cluster

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,16 +1,16 @@
 import React from "react"
-// import Character from './Character'
+import Character from "./Character"
 
-const StarWarsChar = props => {
+const Card = props => {
     console.log(props)
     return (
         <div>
             <h1>StarWars Character:</h1>
-            {starwarsChars.map(character => {
+            {.map(character => {
                 return <Character info={character} />
             })}
         </div>
     )
 }
 
-export default StarWarsChar
+export default Card;
---
+++ starwars/src/components/Character.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Character = ({ characters }) => {
+    return (
+        <div className='character-info'>
+            <h3 className='character-name'>Name: {this.character.name}</h3>
+        </div>
+    )
+}
+
+export default Character
